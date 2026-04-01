ba7f000da8cecf792f0605b50263ccaeeacf0327
Dilkhush Kumar <dk5847001@gmail.com>
2026-04-01 12:37:51 +0530

    operator folder create

---
+++ operators/binaryOperator.java
@@ -0,0 +1,12 @@
+package operators;
+public class binaryOperator {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 5;
+        System.out.println("add = " + (a+b));
+        System.out.println("sub = " + (a-b));
+        System.out.println("multiply = " + (a*b));
+        System.out.println("divide = " + (a/b));
+        System.out.println("reminder = " + (a%b));
+    }
+}
