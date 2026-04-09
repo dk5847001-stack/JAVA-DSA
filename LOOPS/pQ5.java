f56a9c12075052b0b3d5c8d103a3891d3b901c3e
Dilkhush Kumar <dk5847001@gmail.com>
2026-04-09 10:59:42 +0530

    solve pQ.5 completion error

---
+++ LOOPS/pQ5.java
@@ -0,0 +1,10 @@
+package LOOPS;
+
+public class pQ5 {
+    public static void main(String args[]) {
+        for(int i = 0; i<=5; i++){
+            System.out.println("i = "+ i);
+        }
+        System.out.println("i after the loop "+ i);
+    }
+}
