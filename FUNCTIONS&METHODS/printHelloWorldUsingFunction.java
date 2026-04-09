5192ce2777f018fe2734cdebec402464d49beb47
Dilkhush Kumar <dk5847001@gmail.com>
2026-04-09 12:45:03 +0530

    printHelloWorldUsingFunction creaated

---
+++ FUNCTIONS&METHODS/printHelloWorldUsingFunction.java
@@ -0,0 +1,9 @@
+public class printHelloWorldUsingFunction {
+    public static void printHelloWorld() {
+        System.out.println("Hellow World!");
+        System.out.println("Hellow World!");
+    }
+public static void main(String[] args) {
+    printHelloWorld();
+   }    
+}
