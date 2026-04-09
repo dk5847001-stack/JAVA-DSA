3330488c7fc2efa2f706e2d51acd1b6ee3e7abe0
Dilkhush Kumar <dk5847001@gmail.com>
2026-04-09 10:51:49 +0530

    solve pQ.4 multiplaction table

---
+++ LOOPS/pQ4.java
@@ -0,0 +1,13 @@
+package LOOPS;
+import java.util.*;
+public class pQ4 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a number : ");
+        int number = input.nextInt();
+        for(int i = 1; i<=10; i++){
+            System.out.println(number+" X "+i+" = "+number*i);
+        }
+        input.close();
+    }
+}
